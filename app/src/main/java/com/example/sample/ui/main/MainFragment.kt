c275d1f077819183134e7fd7b7b86dab09e739d1
Hsieh Chin Fan <typebrook@gmail.com>
2020-08-03 22:03:41 +0800

    Refactor ViewModel sample

--- app/src/main/java/com/example/sample/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/sample/ui/main/MainFragment.kt
@@ -5,21 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.Navigation
+import androidx.fragment.app.activityViewModels
 import com.example.sample.R
 import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
-    private lateinit var viewModel: MainViewModel
-
-    private val navController by lazy {
-        Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
-    }
+    private val viewModel by activityViewModels<MainViewModel>()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
@@ -27,8 +23,6 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
 
         button.setOnClickListener {
 
