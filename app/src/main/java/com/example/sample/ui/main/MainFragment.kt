8ce3d42e4e82fb24253692178fe6daf668d8aff2
Hsieh Chin Fan <typebrook@gmail.com>
2020-08-27 09:26:13 +0800

    Refactor MainFragment

--- app/src/main/java/com/example/sample/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/sample/ui/main/MainFragment.kt
@@ -21,8 +21,8 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         button.setOnClickListener {
 
