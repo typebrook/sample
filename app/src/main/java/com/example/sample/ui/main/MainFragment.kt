5801ecd1496e93636b58d8ea16f54a1c92936556
Hsieh Chin Fan <pham@topo.tw>
2023-05-11 21:00:30 +0800

    Update plugins and dependencies

--- app/src/main/java/com/example/sample/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/sample/ui/main/MainFragment.kt
@@ -7,24 +7,26 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.sample.R
-import kotlinx.android.synthetic.main.main_fragment.*
+import com.example.sample.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
     private val viewModel by activityViewModels<MainViewModel>()
+    private lateinit var binding: MainFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        binding = MainFragmentBinding.inflate(inflater)
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        button.setOnClickListener {
+        binding.button.setOnClickListener {
 
         }
     }
